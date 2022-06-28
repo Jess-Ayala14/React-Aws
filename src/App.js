19600f2eb3eacbcae991c2e3d6755749d95f9a83
Jess-Ayala14 <jessaydi@gmail.com>
2022-06-28 15:07:44 -0600

    Update and primary condition added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { API, Storage } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import { listBusinesses } from './graphql/queries';
-import { createBusiness as createBusinessMutation, deleteBusiness as deleteBusinessMutation } from './graphql/mutations';
+import { createBusiness as createBusinessMutation, 
+         deleteBusiness as deleteBusinessMutation, 
+         updateBusiness as updateBusinessMutation } from './graphql/mutations';
+
 
 const initialFormState = { name: '', about: '' }
 
-function App() {
+const App = () => {
+  const [hideLightbox, setHideLightbox] = useState(true);
+  const [hideLightbox1, setHideLightbox1] = useState(true);
   const [business, setBusiness] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
+  let [state, setState] = useState(null);
 
   useEffect(() => {
     fetchBusiness();
@@ -37,6 +44,27 @@ function App() {
     }
     setBusiness([...business, formData]);
     setFormData(initialFormState);
+    window.location.reload();
+  }
+
+  async function updateBusiness(){
+    const apiData = await API.graphql({ query: listBusinesses });
+    const BusinessFromAPI = apiData.data.listBusinesses.items;
+    const id_businesses = BusinessFromAPI[0]['id']
+    await Promise.all(BusinessFromAPI.map(async business => {
+      return business;
+    }))
+    if(!formData.name || !formData.about) return;
+    const updateBusiness ={
+      id: id_businesses,
+      name: formData.name,
+      about: formData.about
+    }
+  
+    await API.graphql({query: updateBusinessMutation, variables: {input: updateBusiness}});
+    setBusiness([...business, formData]);
+    setFormData(initialFormState);
+    window.location.reload();
   }
 
   async function deleteBusiness({ id }) {
@@ -55,38 +83,64 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Business List</h1>
-      <input
-        onChange={e => setFormData({ ...formData, 'name': e.target.value })}
-        placeholder="Business name"
-        value={formData.name}
-      />
-      <input
-        onChange={e => setFormData({ ...formData, 'about': e.target.value })}
-        placeholder="about"
-        value={formData.about}
-      />
-      <input
-        type="file"
-        onChange={onChange}
-      />
-      <button onClick={createBusiness}>Save Data</button>
-      <div style={{ marginBottom: 30 }}>
+      <Container>
         {
           business.map(business => (
-            <div key={business.id || business.name}>
-              <h2>{business.name}</h2>
-              <p>{business.about}</p>
-              <button onClick={() => deleteBusiness(business)}>Delete Info</button>
-              {
-                business.image && <img src={business.image} style={{ width: 400 }} />
-              }
+            <div>
+              <Col md="6" key={business.id || business.name}>
+                <h2>{business.name}</h2>
+                <p>{business.about}</p>
+                <Button onClick={() => deleteBusiness(business)}>Delete Info</Button>
+                {
+                  business.image && <img src={business.image} style={{ width: 400 }} />
+                }
+                <Button onClick={() => setHideLightbox(false)}>Edit</Button>
+
+              </Col>
+              <Col className={`${hideLightbox ? "hide-lightbox" : "ligthbox"}`}>
+                <h1>Business List</h1>
+                <input
+                  onChange={e => setFormData({ ...formData, 'name': e.target.value })}
+                  placeholder={"Bus. Name: " + business.name}
+                  value={formData.name}
+                />
+                <input
+                  onChange={e => setFormData({ ...formData, 'about': e.target.value })}
+                  placeholder={"About: " + business.about}
+                  value={formData.about}
+                />
+                <br/>
+                <Button onClick={updateBusiness}>Update</Button>
+                <Button onClick={() => setHideLightbox(true)}>Close</Button>
+              </Col>
             </div>
           ))
         }
-      </div>
+        {business.map(business => (business.id)) == '' &&
+          <Col md="6">
+            <h1>Business List</h1>
+            <input
+              onChange={e => setFormData({ ...formData, 'name': e.target.value })}
+              placeholder="Business name"
+              value={formData.name}
+            />
+            <input
+              onChange={e => setFormData({ ...formData, 'about': e.target.value })}
+              placeholder="about"
+              value={formData.about}
+            />
+            <input
+              type="file"
+              onChange={onChange}
+            />
+            <Button onClick={createBusiness}>Save Data</Button>
+          </Col>
+        }
+      </Container>
+      <br/>
       <AmplifySignOut />
     </div>
+
   );
 }
 
